d3019bed4e8d93c10262f23917f949ab669dcb41
tomato1125 <naokiyoshida1125@icoud.com>
2020-12-28 20:52:25 +0900

    type-aliases

---
+++ src/type-aliases.ts
@@ -0,0 +1,25 @@
+export {};
+
+type Mojiretsu = string;
+
+const fooString: string = 'Hello';
+const fooMojiretus: Mojiretsu = 'Hello';
+
+const example1 = {
+  name: 'Ham',
+  age: 43
+};
+
+type Profile = {
+  name: string;
+  age: number;
+}
+
+const example2: Profile = {
+  name: 'Ham',
+  age: 43
+};
+
+type Profiles2 = typeof example1;
+
+}
